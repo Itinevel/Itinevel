db12b5cf82c8ebe2fad1ea26ab20bc4fd189a664
Salim <salimaloui406@gmail.com>
2024-10-04 13:27:14 +0100

    Adding Client/Server Repos

---
+++ tripplanner/app/layout.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Navbar from "@/components/utils/navbar";
+import { ReactNode, useState } from 'react';
+import { SessionWrapper } from "@/components/sessionwrapper";
+import { useRouter } from 'next/navigation'; // Import useRouter from next/navigation
+import Spinner from "@/components/utils/Spinner"; // Spinner component
+
+const inter = Inter({ subsets: ["latin"] });
+
+interface RootLayoutProps {
+  session: any;
+  children: ReactNode;
+}
+
+const RootLayout = ({ session, children }: RootLayoutProps) => {
+  const [loading, setLoading] = useState(false); // Manage loading state
+  const router = useRouter(); // Get router instance from next/navigation
+
+  const handleNavigation = (path: string) => {
+    setLoading(true);  // Show spinner
+    router.push(path); // Navigate to the new page
+    setTimeout(() => setLoading(false), 500); // Hide spinner after a short delay
+  };
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <SessionWrapper session={session}>
+          {/* Fixed Navbar */}
+          <Navbar className="fixed top-0 left-0 w-full z-50" onNavigate={handleNavigation} />
+
+          {/* Conditionally render spinner when loading */}
+          {loading && (
+            <div className="fixed inset-0 z-50 flex items-center justify-center bg-white bg-opacity-70">
+              <Spinner />
+            </div>
+          )}
+
+          {/* Main content with adjusted padding and spacing */}
+          <main className="min-h-screen     h-full overflow-auto">
+            {children}
+          </main>
+        </SessionWrapper>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
